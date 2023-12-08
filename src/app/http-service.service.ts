9899e6c1337409d432b5594d59e7f125a7a4ee61
aryageorge <aryageorge4@gmail.com>
2023-12-08 18:51:36 +0530

    async and promise-Angular-Arya George

---
+++ src/app/http-service.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http:HttpClient) { }
+
+  getdata() {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  deleteItem(userId: number) {
+    return this.http.delete(`${this.apiUrl}/${userId}`);
+  }
+}
